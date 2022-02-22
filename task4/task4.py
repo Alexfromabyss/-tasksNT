0c9e6c09611d945d2609c35b25072b7859ff2b6c
Alexfromabyss <99674488+Alexfromabyss@users.noreply.github.com>
2022-02-22 10:49:39 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -8,7 +8,7 @@ num, ans = list(), 0
 with open(file_name1, 'r', encoding='utf-8') as input_nums:
     for string in input_nums:
         num.append(int(string.strip()))
-mediana = ceil(sum(num) / len(num))
+mediana = 0 if len(num) == 0 else ceil(sum(num) / len(num))
 for i in num:
     ans += abs(i - mediana)
 print(ans)
